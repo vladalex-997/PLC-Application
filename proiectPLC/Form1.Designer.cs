782fc74136c69694b1c10c111d4fc5da1a2df1a7
Vlad Alex <vladalex997@gmail.com>
2021-09-27 15:23:27 +0300

    commit files

---
+++ proiectPLC/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace proiectPLC
+{
+    partial class FormPLC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLastValue = new System.Windows.Forms.Label();
+            this.textBoxLastValue = new System.Windows.Forms.TextBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.labelError = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelLastValue
+            // 
+            this.labelLastValue.AutoSize = true;
+            this.labelLastValue.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLastValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.labelLastValue.Location = new System.Drawing.Point(975, 21);
+            this.labelLastValue.Name = "labelLastValue";
+            this.labelLastValue.Size = new System.Drawing.Size(420, 68);
+            this.labelLastValue.TabIndex = 6;
+            this.labelLastValue.Text = "Last Read Value";
+            this.labelLastValue.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBoxLastValue
+            // 
+            this.textBoxLastValue.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxLastValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxLastValue.Font = new System.Drawing.Font("Times New Roman", 96F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLastValue.ForeColor = System.Drawing.Color.Yellow;
+            this.textBoxLastValue.Location = new System.Drawing.Point(514, 182);
+            this.textBoxLastValue.Name = "textBoxLastValue";
+            this.textBoxLastValue.ReadOnly = true;
+            this.textBoxLastValue.Size = new System.Drawing.Size(1353, 184);
+            this.textBoxLastValue.TabIndex = 7;
+            this.textBoxLastValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.labelDescription.Location = new System.Drawing.Point(902, 403);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(592, 68);
+            this.labelDescription.TabIndex = 8;
+            this.labelDescription.Text = "Last Value Description ";
+            this.labelDescription.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxDescription.Font = new System.Drawing.Font("Times New Roman", 39F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDescription.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBoxDescription.Location = new System.Drawing.Point(434, 527);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.ReadOnly = true;
+            this.textBoxDescription.Size = new System.Drawing.Size(1463, 399);
+            this.textBoxDescription.TabIndex = 9;
+            this.textBoxDescription.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(778, 929);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(847, 57);
+            this.labelError.TabIndex = 10;
+            this.labelError.Text = "Warning ! Possible Consecutive Values";
+            this.labelError.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.Color.Yellow;
+            this.labelStatus.Location = new System.Drawing.Point(74, 939);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(380, 46);
+            this.labelStatus.TabIndex = 11;
+            this.labelStatus.Text = "Status Conexiune PLC";
+            // 
+            // FormPLC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1924, 1023);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.textBoxLastValue);
+            this.Controls.Add(this.labelLastValue);
+            this.Name = "FormPLC";
+            this.Text = "PLC Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormPLC_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelLastValue;
+        private System.Windows.Forms.TextBox textBoxLastValue;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ proiectPLC/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using S7.Net;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace proiectPLC
+{
+    
+    public partial class FormPLC : Form
+    {
+        public Timer timer1;
+        public Timer timer2;
+        public Timer timer3;
+        public Plc plc = null;
+        public  object current;
+        public  object previous="Default";
+        public int nrlafel;
+       
+
+
+
+        [DllImport("user32.dll")]
+        static extern bool HideCaret(IntPtr hWnd);
+       
+
+        void textBoxLastValue_GotFocus(object sender, EventArgs e)
+        {
+            HideCaret(textBoxLastValue.Handle);
+        }
+
+        public FormPLC()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDeconectarePLC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                plc.Close();
+                if (!plc.IsConnected)
+                {
+                    MessageBox.Show("PLC deconectat");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("PLC nu a fost deconectat");
+            }
+            
+           
+        }
+
+        public void ConectarePLC()
+        {
+            try
+            {
+                string Rack = "0";
+                string Slot = "0";
+                string Ip = "10.21.7.217";
+
+
+                CpuType cpu = (CpuType)Enum.Parse((typeof(CpuType)), "S71200");
+
+                plc = new Plc(cpu, Ip, Convert.ToInt16(Rack), Convert.ToInt16(Slot));
+
+
+
+                plc.Open();
+            }
+             catch (Exception)
+            {
+
+            }
+                
+
+                //if (plc.IsConnected)
+                //{
+                //    MessageBox.Show("PLC conectat");
+                //}
+           
+
+        }
+
+        private void buttonConectarePLC_Click(object sender, EventArgs e)
+        {
+
+            ConectarePLC();
+            
+        }
+
+        /*
+
+
+
+Database databaseObject = new Database();
+databaseObject.OpenConnection();
+
+string query = "SELECT * from PLCRecords WHERE Id="+2;
+SqlCommand mycommand = new SqlCommand(query,databaseObject.myConnection);
+string rez = "";
+SqlDataReader re = mycommand.ExecuteReader();
+if (re.HasRows)
+{
+
+    while (re.Read())
+    {
+        rez = re[2].ToString();
+
+    }
+}
+re.Close();
+     MessageBox.Show(rez);
+    databaseObject.CloseConnection();
+
+*/
+
+        private void FormPLC_Load(object sender, EventArgs e)
+        {
+            labelStatus.Text = "";
+            textBoxLastValue.GotFocus += new EventHandler(textBoxLastValue_GotFocus);
+            ConectarePLC();
+            InitTimer3();
+            InitTimer1();
+            InitTimer2();
+           
+         
+        }
+
+        public void InitTimer1()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 500; // nr sec in mili
+            timer1.Start();
+
+           
+
+
+        }
+
+        public void InitTimer2()
+        {
+            timer2 = new Timer();
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Interval = 1000; // nr sec in mili
+            timer2.Start();
+        }
+
+        public void InitTimer3()
+        {
+            timer3 = new Timer();
+            timer3.Tick += new EventHandler(timer3_Tick);
+            timer3.Interval = 200; // nr sec in mili
+            timer3.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            readPLC();
+            RewriteValue();
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            DisplayPLC();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            checkConnection();
+        }
+
+        public void checkConnection()
+        {
+            if (plc.IsConnected)
+            {
+                labelStatus.Text = "PLC CONNECTED";
+            }
+            else
+            {
+                labelStatus.Text = "PLC NOT CONNECTED. RECONNECTING...";
+                ConectarePLC();
+            }
+        }
+
+        public void readPLC()
+        {
+            try
+            {
+                current = plc.Read(DataType.DataBlock, 10, 77, VarType.String, 17);
+            }
+            catch (Exception)
+            {
+
+            }
+
+              
+
+              
+           
+            
+               
+           
+               
+                
+
+        }
+
+        public void RewriteValue()
+        {
+            
+                if (current.ToString().Contains("8") == true)
+                {
+                
+                if (previous.ToString() == current.ToString())
+                {
+                    nrlafel++;
+                }
+                else
+                {
+                    nrlafel = 0;
+                }
+
+
+                previous = current;
+
+             
+                }
+            
+            
+
+           
+      
+            
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public void DisplayPLC()
+        {
+            try
+            {
+                textBoxLastValue.Text = previous.ToString();
+
+                if (previous.ToString().Contains("8") == true)
+                {
+
+                    Database databaseObject = new Database();
+                    databaseObject.OpenConnection();
+                    string temporary = "Default";
+
+                    string rev = Reverse(previous.ToString());
+                    string gog = rev.Remove(0, 6);
+                    string fin = Reverse(gog);
+                    string replacement = fin.Trim('\r', '\n');
+
+                    // string[] lines = previous.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                    // string address = lines[0];  // or safe: lines.ElementAtOrDefault(3)  
+
+
+                    string query = "SELECT KitName from PLCRecords WHERE SeatCodeRear='" + replacement + "'";
+                    SqlCommand myCommand = new SqlCommand(query, databaseObject.myConnection);
+                    //  myCommand.Parameters.AddWithValue("@SeatCodeRear",previous.ToString());
+
+
+
+
+                    //  MessageBox.Show(address);
+
+
+
+                    //  System.IO.File.WriteAllText(@"C:\test\path.txt", replacement);
+
+
+                    temporary = (string)myCommand.ExecuteScalar().ToString();
+                    //string rez = "";
+                    //SqlDataReader re = myCommand.ExecuteReader();
+                    //if (re.HasRows)
+                    //{
+
+                    //    while (re.Read())
+                    //    {
+                    //        rez = re[1].ToString();
+
+                    //    }
+                    //}
+                    //re.Close();
+
+                    databaseObject.CloseConnection();
+
+                    textBoxDescription.Text = temporary;
+
+                    // MessageBox.Show(temporary);
+
+                    if (nrlafel >= 3600)
+                    {
+
+                        labelError.Visible = true;
+                    }
+                    else
+                    {
+                        labelError.Visible = false;
+                    }
+
+
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        
+
+
+
+
+        }
+     
+   
+       
+        private void buttonCitireAdresa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+      
+    }
+}
